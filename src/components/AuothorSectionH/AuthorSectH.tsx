d4733f30ea0bd1fd200b9bd408a41d5f9e70db12
Simfort <lemondzavadavid265@gmail.com>
2024-11-17 21:42:18 +0300

    added:AuothorSectionH

---
+++ src/components/AuothorSectionH/AuthorSectH.tsx
@@ -0,0 +1,22 @@
+
+import { useRef } from 'react'
+import './AuothorSectH.scss'
+export default function AuothorSectH() {
+    const light = useRef<any>()
+    const section = useRef<any>()
+    const mouseMoveHandler = (e: any) => {
+        light.current.style.left = `${e.clientX - 50}px`
+        light.current.style.top = `${e.clientY - 500}px`
+    }
+    return <section ref={section} onPointerDown={mouseMoveHandler} onMouseMove={mouseMoveHandler} className='section-author'>
+        <div ref={light} className='light'></div>
+        <h2 className='section-author__h2'>Who`s author this site? </h2>
+        <p className='section-author__text'>I am David,i am 14 years old.I living in Russia,Simferopol.I love programmin and i know frontend develop.This site created for my portfolio.This site created on <ul>
+            <li>HTML/CSS</li>
+            <li>JS/TS</li>
+            <li>React and react-router</li>
+            <li>Git</li>
+        </ul></p>
+        <p className='section-author__hiddenText' >It not all sites.You can search they here on my github Simfart!</p>
+    </section>
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,9 @@
 import AboutSectH from "../components/AboutSectionH/AboutSect";
+import AuothorSectH from "../components/AuothorSectionH/AuthorSectH";
 
 export default function Home() {
     return <main className="home-main">
         <AboutSectH />
+        <AuothorSectH />
     </main>
 }
