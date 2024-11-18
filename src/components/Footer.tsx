0119ae5ede948306d019bb07e380e20108a63bf4
Simfort <lemondzavadavid265@gmail.com>
2024-11-18 19:49:13 +0300

    modificated:fix style

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,10 +5,13 @@ import './Footer.scss';
 
 export default function Footer() {
     return <footer className="footer">
+        <div className="footer-text">
+            <p>I am David</p>
+        </div>
         <div className="footer-panel__link">
-            <a className="link-github" href="https://github.com/Simfort"><img width={50} src={github} alt="" /></a>
-            <a className="link-telegram" href="https://t.me/getcode_front"><img width={50} src={telegram} alt="" /></a>
-            <a className="link-youtube" href="https://youtube.com/@simfort_iron?si=T8KHCiBLqgH_sybe"><img width={50} src={youtube} alt="" /></a>
+            <a className="link-github" href="https://github.com/Simfort"><img height={50} width={50} src={github} alt="" /></a>
+            <a className="link-telegram" href="https://t.me/getcode_front"><img height={50} width={50} src={telegram} alt="" /></a>
+            <a className="link-youtube" href="https://youtube.com/@simfort_iron?si=T8KHCiBLqgH_sybe"><img height={50} width={50} src={youtube} alt="" /></a>
         </div>
     </footer>
 }
